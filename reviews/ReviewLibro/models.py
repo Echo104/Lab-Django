fce46ed229064f12783601b871f0cdaaec940dce
Kazel 10 <echoquequi@unsa.edu.pe>
2024-11-23 21:05:25 -0500

    creacion de los modelos en ReviewLibro

--- reviews/ReviewLibro/models.py
+++ reviews/ReviewLibro/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=150)
+    autor = models.CharField(max_length=50)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.titulo
+
+class Usuario(models.Model):
+    user = models.CharField(max_length=30)
+
+class Reseña(models.Model):
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    calificacion = models.IntegerField() 
+    comentario = models.TextField()
+
+    def __str__(self):
+        return f'Reseña de {self.usuario} para {self.libro}'
